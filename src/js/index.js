8697537d7156682a1aa73b90679fa060a7d93223
RuanCarlosDeJesus <133722831+RuanCarlosDeJesus@users.noreply.github.com>
2024-02-01 21:08:52 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+alert("oi")
+
+
+
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+   
+  
+});
